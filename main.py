03dac008855a408a57ae255458ec79cbd13172b6
Gergobergo0 <157916533+Gergobergo0@users.noreply.github.com>
2024-11-23 16:48:44 +0100

    Update main.py
    .

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
 
     transform = transforms.Compose([
         transforms.Resize((128, 128)),
-        transforms.RandomHorizontalFlip(),
+        transforms.RandomHorizontalFlip(0.5),
+        transforms.RandomVerticalFlip(0.5),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5], std=[0.5])  # Az adatok 0-1 közé kerülnek
     ])
@@ -29,8 +30,8 @@ if __name__ == "__main__":
     val_size = len(train_dataset) - train_size
     train_data, val_data = random_split(train_dataset, [train_size, val_size])
 
-    train_loader = DataLoader(train_data, batch_size=16, shuffle=True)
-    val_loader = DataLoader(val_data, batch_size=16, shuffle=False)
+    train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
+    val_loader = DataLoader(val_data, batch_size=64, shuffle=False)
 
     # Modell inicializálása
     model = FocusNet()
